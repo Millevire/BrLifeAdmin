a3557e25842ff28ee8ea91bd8e179d5275f3564f
millefire <ignacio.cortes.17@gmail.com>
2019-05-30 20:52:02 -0400

    20:51

--- app/src/main/java/com/example/esteban/brlifeadmin/AdminActivity.java
+++ app/src/main/java/com/example/esteban/brlifeadmin/AdminActivity.java
@@ -15,7 +15,7 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-        //Hola 40404
+        //Hola 40333
         btnTipoProducto=(Button)findViewById(R.id.btnTipoProducto);
         btnRegiones=(Button)findViewById(R.id.btnRegiones);
         btnRol=(Button)findViewById(R.id.btnRol);
